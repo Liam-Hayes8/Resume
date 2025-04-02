a24b429de2075511d140f7c2e364cbdd8387983b
Liam-Hayes8 <161670473+Liam-Hayes8@users.noreply.github.com>
2025-04-02 14:39:48 -0700

    Delete 317043667 directory

--- 317043667/processRequests.h
+++
@@ -1,7 +0,0 @@
-#ifndef PROCESSREQUESTS_H
-#define PROCESSREQUESTS_H
-
-//Declaration for the consumer function (processes buffered requests)
-void* processRequests(void* arg);
-
-#endif // PROCESSREQUESTS_H
--- 317043667/receiveAndBufferRequests.h
+++
@@ -1,7 +0,0 @@
-#ifndef RECEIVEANDBUFFERREQUESTS_H
-#define RECEIVEANDBUFFERREQUESTS_H
-
-//Declatration for the producer function (read and buffer requests)
-void* receiveAndBufferRequests(void* arg);
-
-#endif // RECEIVEANDBUFFERREQUESTS_H
