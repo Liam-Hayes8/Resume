293b8c558b82a9116f56958d8ff3e4953c3223a1
Liam-Hayes8 <161670473+Liam-Hayes8@users.noreply.github.com>
2025-04-02 14:38:07 -0700

    Add files via upload
    Semaphore Project

---
+++ 317043667/processRequests.h
@@ -0,0 +1,7 @@
+#ifndef PROCESSREQUESTS_H
+#define PROCESSREQUESTS_H
+
+//Declaration for the consumer function (processes buffered requests)
+void* processRequests(void* arg);
+
+#endif // PROCESSREQUESTS_H
